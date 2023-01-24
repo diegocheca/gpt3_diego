ed87f9f4aa54f5cc2fb2c36fc6e83b2464cb78cf
git@git.assembla.com <git@git.assembla.com>
2023-01-24 10:19:30 -0300

    Finishing the page

--- src/containers/blog/blog.jsx
+++ src/containers/blog/blog.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
+import { blog01, blog02, blog03, blog04, blog05 } from './import';
+import { Article } from '../../components';
 import './blog.css';
 
 const blog = () => {
   return (
-    <div>blog</div>
+    <div className='gpt3__blog section__padding' id="blog">
+      <div className='gpt3__blog-heading'>
+        <h1 className='gradient__text'>A lot is happing, We are blogging about it.</h1>
+      </div>
+      <div className='gpt3__blog-container'>
+        <div className='gpt3__blog-container_groupA'>
+          <Article imgUrl={blog01} date="Sep 20,1990" title="New Article"/>
+        </div>
+        <div className='gpt3__blog-container_groupB'>
+        <Article imgUrl={blog02} date="Sep 20,1990" title="New Article"/>
+        <Article imgUrl={blog03} date="Sep 20,1990" title="New Article"/>
+        <Article imgUrl={blog04} date="Sep 20,1990" title="New Article"/>
+        <Article imgUrl={blog05} date="Sep 20,1990" title="New Article"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
